241cc0a25382d632757c482458b7106a24cb4092
Владислав Овчинников <vladovchinnikov950@gmail.com>
2020-05-10 13:04:03 +0300

    Изменил команду выполняемую при sentry

--- sentry.sh
+++ sentry.sh
@@ -8,20 +8,3 @@ TASK_ID=$(${DOCKER_CMD} service ps --filter 'desired-state=running' $SERVICE_NAM
 NODE_ID=$(${DOCKER_CMD} inspect --format '{{ .NodeID }}' $TASK_ID)
 CONTAINER_ID=$(${DOCKER_CMD} inspect --format '{{ .Status.ContainerStatus.ContainerID }}' $TASK_ID)
 TASK_NAME=swarm_exec_${RANDOM}
-
-TASK_ID=$(${DOCKER_CMD} service create \
-  --detach \
-  --name=${TASK_NAME} \
-  --restart-condition=none \
-  --mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
-  --constraint node.id==${NODE_ID} \
-  docker:latest docker exec ${CONTAINER_ID} "sentry upgrade")
-
-while : ; do
-  STOPPED_TASK=$(${DOCKER_CMD} service ps --filter 'desired-state=shutdown' ${TASK_ID} -q)
-  [[ ${STOPPED_TASK} != "" ]] && break
-  sleep 1
-done
-
-${DOCKER_CMD} service logs --raw ${TASK_ID}
-${DOCKER_CMD} service rm ${TASK_ID} > /dev/null
