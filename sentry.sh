b89b9c4dc8dc617de9fc69d089c204369d4194b5
Владислав Овчинников <vladovchinnikov950@gmail.com>
2020-05-10 13:02:29 +0300

    Изменил команду выполняемую при sentry

--- sentry.sh
+++ sentry.sh
@@ -2,11 +2,26 @@
 set -e
 
 SERVICE_NAME=socshared_sentry
+DOCKER_CMD=docker
 
-TASK_ID=$(sudo docker service ps --filter 'desired-state=running' $SERVICE_NAME -q)
-NODE_ID=$(sudo docker inspect --format '{{ .NodeID }}' $TASK_ID)
-CONTAINER_ID=$(sudo docker inspect --format '{{ .Status.ContainerStatus.ContainerID }}' $TASK_ID)
-NODE_HOST=$(sudo docker node inspect --format '{{ .Description.Hostname }}' $NODE_ID)
-export DOCKER_HOST="tcp://$NODE_HOST:2376"
-sudo docker exec -it $CONTAINER_ID "sentry upgrade"
-sudo docker exec -it $CONTAINER_ID "sentry createuser --email vladovchinnikov950@gmail.com --password admin --superuser --no-input"
+TASK_ID=$(${DOCKER_CMD} service ps --filter 'desired-state=running' $SERVICE_NAME -q)
+NODE_ID=$(${DOCKER_CMD} inspect --format '{{ .NodeID }}' $TASK_ID)
+CONTAINER_ID=$(${DOCKER_CMD} inspect --format '{{ .Status.ContainerStatus.ContainerID }}' $TASK_ID)
+TASK_NAME=swarm_exec_${RANDOM}
+
+TASK_ID=$(${DOCKER_CMD} service create \
+  --detach \
+  --name=${TASK_NAME} \
+  --restart-condition=none \
+  --mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
+  --constraint node.id==${NODE_ID} \
+  docker:latest docker exec ${CONTAINER_ID} "sentry upgrade")
+
+while : ; do
+  STOPPED_TASK=$(${DOCKER_CMD} service ps --filter 'desired-state=shutdown' ${TASK_ID} -q)
+  [[ ${STOPPED_TASK} != "" ]] && break
+  sleep 1
+done
+
+${DOCKER_CMD} service logs --raw ${TASK_ID}
+${DOCKER_CMD} service rm ${TASK_ID} > /dev/null
