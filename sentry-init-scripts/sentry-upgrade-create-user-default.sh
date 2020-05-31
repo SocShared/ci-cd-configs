f2b91fa223bf087492cc8e32db8adb1276551095
Владислав Овчинников <vladovchinnikov950@gmail.com>
2020-05-31 23:50:09 +0300

    Поправил cкрипт инициализации Sentry

--- sentry-init-scripts/sentry-upgrade-create-user-default.sh
+++ sentry-init-scripts/sentry-upgrade-create-user-default.sh
@@ -1,8 +1,2 @@
-if [ ! -f .initialized ]; then
-    echo "Initializing container"
-    # run initializing commands
-    touch .initialized
-fi
-
 exec sentry upgrade --noinput
 exec sentry createuser --email admin@local.ru --password Admin12345 --superuser --no-input
